b63bc2b7f31e62e91b0861682b57e63a6c43b3bc
danielspagna-dot <danielspagna@gmail.com>
2026-07-23 21:28:26 +0200

    lab

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,15 +1,29 @@
-// const createError = require("http-errors");
-// const Comment = require("../lib/models/comment.model");
-
-// TODO Iteracion 5 [Bonus]: Implementar cada funcion y exportarlas al final del archivo
+const createError = require("http-errors");
+const Comment = require("../lib/models/comment.model");
 
 const create = async (req, res, next) => {
-  // TODO: Crear comentario (author = req.user._id, post = req.params.id) → 201
+  try {
+    const comment = await Comment.create({
+      body: req.body.body,
+      author: req.user._id,
+      post: req.params.id,
+    });
+    res.status(201).json(comment);
+  } catch (error) {
+    next(error);
+  }
 };
 
 const remove = async (req, res, next) => {
-  // TODO: Eliminar comentario por req.params.commentId
-  // TODO: Si no existe → 404, si existe → 204
+  try {
+    const comment = await Comment.findByIdAndDelete(req.params.commentId);
+    if (!comment) {
+      return next(createError(404, "Comment not found"));
+    }
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { create, remove };
--- src/middlewares/auth.mid.js
+++ src/middlewares/auth.mid.js
@@ -1,17 +1,19 @@
 const createError = require("http-errors");
-// const User = require("../lib/models/user.model");
+const User = require("../lib/models/user.model");
 
 module.exports = async (req, res, next) => {
   try {
-    // TODO Iteracion 2: Verificar que req.session.userId existe
-    //   Si no existe → next(createError(401, "session not found"))
+    if (!req.session.userId) {
+      return next(createError(401, "session not found"));
+    }
 
-    // TODO Iteracion 2: Buscar el usuario en la BD con req.session.userId
-    //   Si no existe → next(createError(401, "session user not found"))
+    const user = await User.findById(req.session.userId);
+    if (!user) {
+      return next(createError(401, "session user not found"));
+    }
 
-    // TODO Iteracion 2: Asignar el usuario a req.user y llamar a next()
-
-    next(); // elimina esta linea cuando implementes el middleware
+    req.user = user;
+    next();
   } catch (error) {
     next(error);
   }
